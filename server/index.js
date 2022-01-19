1c7386972ad64556e72e287b9f1663ff359196f7
Mehdi Safari <sa.mehdisafari@gmail.com>
2022-01-19 09:31:12 -0800

    finish server side

--- server/index.js
+++ server/index.js
@@ -1,82 +1,30 @@
 require('dotenv').config()
-const {CONNECTION_STRING} = process.env
-const Sequelize = require('sequelize')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const {SERVER_PORT} = process.env
+const {
+    getAllClients,
+    getPendingAppointments,
+    getUpcomingAppointments,
+    getPastAppointments, 
+    approveAppointment, 
+    completeAppointment, 
+    deleteAppointment
+} = require('./controller.js')
 
-// you wouldn't want to rejectUnauthorized in a production app, but it's great for practice
-const sequelize = new Sequelize(CONNECTION_STRING, {
-    dialect: 'postgres', 
-    dialectOptions: {
-        ssl: {
-            rejectUnauthorized: false
-        }
-    }
-})
+app.use(express.json())
+app.use(cors())
 
-let nextEmp = 5
+// USERS
+app.get('/clients', getAllClients)
 
-module.exports = {
-    getAllClients: (req, res) => {
-        sequelize.query(`select * from cc_clients c
-        join cc_users u on c.user_id = u.user_id;`)
-            .then(dbRes => res.status(200).send(dbRes[0]))
-            .catch(err => console.log(err))
-    }, 
-    
-    getPendingAppointments: (req, res) => {
-        sequelize.query(`select * from cc_appointments
-        where approved = false
-        order by date desc;`)
-            .then(dbRes => res.status(200).send(dbRes[0]))
-            .catch(err => console.log(err))
-    },
+// APPOINTMENTS
+app.get('/pending', getPendingAppointments)
+app.get('/upcoming', getUpcomingAppointments)
+app.get('/appt', getPastAppointments)
+app.put('/approve', approveAppointment)
+app.put('/complete', completeAppointment)
+app.delete('/appt', deleteAppointment)
 
-    getUpcomingAppointments: (req, res) => {
-        sequelize.query(`select a.appt_id, a.date, a.service_type, a.approved, a.completed, u.first_name, u.last_name 
-        from cc_appointments a
-        join cc_emp_appts ea on a.appt_id = ea.appt_id
-        join cc_employees e on e.emp_id = ea.emp_id
-        join cc_users u on e.user_id = u.user_id
-        where a.approved = true and a.completed = false
-        order by a.date desc;`)
-            .then(dbRes => res.status(200).send(dbRes[0]))
-            .catch(err => console.log(err))
-    },
-
-    getPastAppointments: (req, res) => {
-        sequelize.query(`select a.appt_id, a.date, a.service_type, a.notes, u.first_name, u.last_name 
-        from cc_appointments a
-        join cc_emp_appts ea on a.appt_id = ea.appt_id
-        join cc_employees e on e.emp_id = ea.emp_id
-        join cc_users u on e.user_id = u.user_id
-        where a.approved = true and a.completed = true
-        order by a.date desc;`)
-            .then(dbRes => res.status(200).send(dbRes[0]))
-            .catch(err => console.log(err))
-    },
-
-    approveAppointment: (req, res) => {
-        let {apptId} = req.body
-
-        sequelize.query(`update cc_appointments set approved = true
-        where appt_id = ${apptId};
-        
-        insert into cc_emp_appts (emp_id, appt_id)
-        values (${nextEmp}, ${apptId}),
-        (${nextEmp + 1}, ${apptId});
-        `)
-            .then(dbRes => {
-                res.status(200).send(dbRes[0])
-                nextEmp += 2
-            })
-            .catch(err => console.log(err))
-    },
-
-    completeAppointment: (req, res) => {
-        let {apptId} = req.body
-
-        sequelize.query(`update cc_appointments set completed = true
-        where appt_id = ${apptId};`)
-            .then(dbRes => res.status(200).send(dbRes[0]))
-            .catch(err => console.log(err))
-    }
-}
+app.listen(SERVER_PORT, () => console.log(`up on ${SERVER_PORT}`))
